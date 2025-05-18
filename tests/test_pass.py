a11515fbb3d151789ed6c13d14177dd72c40cc3b
Rustam Teplukhin <teplukhinrg@student.bmstu.ru>
2025-05-18 16:48:24 +0300

    Add more stable unhappy tests

--- tests/test_pass.py
+++ tests/test_pass.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pages.model.user_model import UserModel
 
 class TestAddUser:
@@ -75,3 +77,58 @@ class TestAddUser:
         add_login_page.clean_form()
         assert "Поле обязательно" in result_user, "Ошибочное добавление пользователя"
 
+    @pytest.mark.xfail(condition=True, reason="Поле 'Пол' не содержит проверку на корректность значения")
+    def test_unhappy_path_4(self, register_admin, add_login_page):
+        '''
+        Тест 7. Проверка добавления пользователя с обязательными полями, но
+        заполнением поля "Пол" значением "Жен"
+        '''
+
+        user_data = UserModel().random_user()
+        user_data["gender"] = "Жен"
+        add_login_page.add_user_info(data=user_data)
+        result_user = add_login_page.get_add_result()
+        add_login_page.clean_form()
+        assert "Поле заполнено некорректно!" in result_user, "Ошибочное добавление пользователя"
+
+    @pytest.mark.xfail(condition=True, reason="Поле 'Пол' не содержит проверку на корректность значения")
+    def test_unhappy_path_5(self, register_admin, add_login_page):
+        '''
+        Тест 8. Проверка добавления пользователя с обязательными полями, но
+        заполнением поля "Пол" значением "Муж"
+        '''
+
+        user_data = UserModel().random_user()
+        user_data["gender"] = "Муж"
+        add_login_page.add_user_info(data=user_data)
+        result_user = add_login_page.get_add_result()
+        add_login_page.clean_form()
+        assert "Поле заполнено некорректно!" in result_user, "Ошибочное добавление пользователя"
+
+    @pytest.mark.xfail(condition=True, reason="Поле 'Возраст' не содержит проверку на положительность значения")
+    def test_unhappy_path_6(self, register_admin, add_login_page):
+        '''
+        Тест 9. Проверка добавления пользователя с обязательными полями, но
+        заполнением поля "Возраст" значением "-1"
+        '''
+
+        user_data = UserModel().random_user()
+        user_data["age"] = -1
+        add_login_page.add_user_info(data=user_data)
+        result_user = add_login_page.get_add_result()
+        add_login_page.clean_form()
+        assert "Поле заполнено некорректно!" in result_user, "Ошибочное добавление пользователя"
+
+    @pytest.mark.xfail(condition=True, reason="Поле 'Пол' не является обязательным")
+    def test_unhappy_path_7(self, register_admin, add_login_page):
+        '''
+        Тест 10. Проверка добавления пользователя с обязательными полями, но
+        без заполнения поля "Пол"
+        '''
+
+        user_data = UserModel().random_user()
+        user_data["gender"] = None
+        add_login_page.add_user_info(data=user_data)
+        result_user = add_login_page.get_add_result()
+        add_login_page.clean_form()
+        assert "Поле заполнено некорректно!" in result_user, "Ошибочное добавление пользователя"
